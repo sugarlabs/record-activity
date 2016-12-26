d1241eea13e57ee39528e30180b38807df9eae38
James Cameron <quozl@laptop.org>
2016-12-27 07:05:28 +1100

    Add copyright and license to all source files
    For each file without an explicit copyright and license, the git
    history was reviewed to find the implied copyright and license on the
    assumption that the contributors intended to either assign their
    copyright to the original author or to One Laptop per Child.
    
    Signed-off-by: James Cameron <quozl@laptop.org>

--- glive.py
+++ glive.py
@@ -1,4 +1,5 @@
-#Copyright (c) 2008, Media Modifications Ltd.
+#Copyright (C) 2008, Media Modifications Ltd.
+#Copyright (C) 2011, One Laptop per Child
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
