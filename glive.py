2a35ca45021f188d435cb3d9a191a4d7f960b0c5
James Cameron <quozl@laptop.org>
2018-06-15 13:55:19 +1000

    Revert "Add copyright and license to all source files"
    This reverts commit d1241eea13e57ee39528e30180b38807df9eae38.

--- glive.py
+++ glive.py
@@ -1,5 +1,4 @@
-#Copyright (C) 2008, Media Modifications Ltd.
-#Copyright (C) 2011, One Laptop per Child
+#Copyright (c) 2008, Media Modifications Ltd.
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
